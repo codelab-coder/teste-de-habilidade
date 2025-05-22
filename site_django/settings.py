c201cd1a1e638ef1cd04ad27c5dc226d284cad0d
codelab-coder <nandaomguimaraes@gmail.com>
2025-05-22 12:53:23 -0300

    update

--- site_django/settings.py
+++ site_django/settings.py
@@ -74,6 +74,12 @@ WSGI_APPLICATION = 'site_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
